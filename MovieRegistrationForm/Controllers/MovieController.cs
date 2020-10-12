b16c04bc03c2c33b813f48beb52d7c7ec2a1b4e1
TayloreJames <67704935+TayloreJames@users.noreply.github.com>
2020-10-12 19:31:04 -0400

    Figured it out

---
+++ MovieRegistrationForm/Controllers/MovieController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MovieRegistrationForm.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MovieRegistrationForm.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MovieRegistrationResult(MovieRegistrationModel model)
+        {
+            
+            return View(model);
+        }
+
+
+    }
+}
---
+++ MovieRegistrationForm/Models/MovieRegistrationModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace MovieRegistrationForm.Models
+{
+    public class MovieRegistrationModel
+    {
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Genre { get; set; }
+
+        public int Year { get; set; }
+
+        public string Actors { get; set; }
+
+        public string Directors { get; set; }
+
+    }
+}
